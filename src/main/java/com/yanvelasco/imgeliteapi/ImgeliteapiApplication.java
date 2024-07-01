2cd342ee0431bb3fb5cff616456edfe7d3027968
Yan Velasco <yancarlosduartevelasco@gmail.com>
2024-07-01 14:50:13 -0300

    Enable EnableJpaAuditing in ImgeliteapiApplication

--- src/main/java/com/yanvelasco/imgeliteapi/ImgeliteapiApplication.java
+++ src/main/java/com/yanvelasco/imgeliteapi/ImgeliteapiApplication.java
@@ -2,8 +2,10 @@ package com.yanvelasco.imgeliteapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ImgeliteapiApplication {
 
     public static void main(String[] args) {
