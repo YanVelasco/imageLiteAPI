553ffe884bfd52d66b6e472eab199fd47f4cb7c4
Yan Velasco <yancarlosduartevelasco@gmail.com>
2024-07-01 11:40:48 -0300

    Creating the application

---
+++ src/main/java/com/yanvelasco/imgeliteapi/ImgeliteapiApplication.java
@@ -0,0 +1,13 @@
+package com.yanvelasco.imgeliteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgeliteapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImgeliteapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yanvelasco/imgeliteapi/ImgeliteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanvelasco.imgeliteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgeliteapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
