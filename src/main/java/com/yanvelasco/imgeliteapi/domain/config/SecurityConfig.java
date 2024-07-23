0a246aaf92d0d8f32a91c792b36e01679579c254
Yan Velasco <yancarlosduartevelasco@gmail.com>
2024-07-22 21:30:43 -0300

    Finalizing the filter control

--- src/main/java/com/yanvelasco/imgeliteapi/domain/config/SecurityConfig.java
+++ src/main/java/com/yanvelasco/imgeliteapi/domain/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.yanvelasco.imgeliteapi.domain.config;
 
+import com.yanvelasco.imgeliteapi.domain.config.userFilter.JwtFilter;
+import com.yanvelasco.imgeliteapi.domain.userDomain.jwt.JWTService;
+import com.yanvelasco.imgeliteapi.domain.userDomain.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +11,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -15,13 +19,19 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public JwtFilter jwtFilter(JWTService jwtService, UserRepository userRepository) {
+        return new JwtFilter(jwtService, userRepository);
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, JwtFilter jwtFilter) throws Exception {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
@@ -29,8 +39,8 @@ public class SecurityConfig {
                             auth.requestMatchers("/v1/users/**").permitAll();
                             auth.anyRequest().authenticated();
                         }
-                );
-
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 
